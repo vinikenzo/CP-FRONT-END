8f0bf3898e445128d308693b8b3f6e799643b674
JoaoVictor087 <joaovictoralves087@gmail.com>
2025-04-09 09:33:38 -0300

    adição do json com as receitas prontas

--- cp-front/src/app/layout.tsx
+++ cp-front/src/app/layout.tsx
@@ -1,8 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,9 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        
-      >
+      <body>
         {children}
       </body>
     </html>
